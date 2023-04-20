10d6296c9d19f5a346c9b4e7a268f379484833a8
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-04-19 21:44:51 -0300

    Create ex03.java

---
+++ exercicios04/ex03.java
@@ -0,0 +1,19 @@
+package exercicios04;
+import java.util.Scanner;
+/*FAÇA UM PROGRAMA PARA LER UM VALOR INTEIRO
+ (ACEITAR SOMENTE VALORES ENTRE 1 E 10)E ESCREVA A TABUADA DO VALOR LIDO.
+*/
+
+public class ex03 {
+    public static void main(String[] args){
+    Scanner teclado = new Scanner(System.in);
+  
+
+
+    System.out.println("Digite um numero entre 1 e 10: ");
+    int numero = teclado.nextInt();
+    
+    
+}
+
+}
