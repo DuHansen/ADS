252e1bcbd78cadb1a47bd4f1cd14ccb465207cb1
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-04-19 21:28:34 -0300

    Update ex02.java

--- exercicios04/ex02.java
+++ exercicios04/ex02.java
@@ -1,22 +1,21 @@
-package exercicios04;
+package app;
 
-
-/*FAÇA  UM  PROGRAMA  QUE  IMPRIMA  OS  NÚMEROS  DE 1A 10EM  ORDEM  CRESCENTE E 
- DEPOIS  EM  ORDEM DECRESCENTE.(NÃO POSSUI INTERAÇÃO COM O USUÁRIO)
-*/
-
-public class ex02 {
-    public void main(String[] args) {
-        
+public class App {
+    public static void main(String[] args) {
         int i;
-        for(i = 0; i <10; i++){
-            System.out.println("Numeros em ordem crescente: " + i);
+        System.out.println("Numeros em ordem crescente: ");
+        for(i = 1; i <11; i++){
+            System.out.println("São:"+ i);
+        }
+        System.out.println("/////////////////");
+        System.out.println("Numeros em ordem decrescente: ");
+        int x = 10;
+        while(x >= 1){
+            System.out.println("São: " + x);
+            x -- ;
         }
         
-        int x;
-        for(x = 0; x > 10; x++){
-            System.out.println("Numeros em ordem decrescente: " + i);
-        }
 
     }
+
 }
