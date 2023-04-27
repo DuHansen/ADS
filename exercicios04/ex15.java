fe90812eb8fbdec45e42e8a471a5954583962831
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-04-26 21:47:59 -0300

    Update ex15.java

--- exercicios04/ex15.java
+++ exercicios04/ex15.java
@@ -8,5 +8,75 @@ FINAL O PROGRAMA DEVE INFORMAR:
 • QUAL A CATEGORIA COM MENOS GASTOS;
  */
 public class ex15 {
-    
+
+            public static void main(String[] args) {
+            Scanner teclado = new Scanner(System.in);
+        
+            double totalAlimentacao = 0;
+            double totalCombustivel = 0;
+            double totalsaude = 0;
+            int continuar = 0;
+
+            do{
+                
+                System.out.print("Informe o valor do gasto: R$ " );
+                double gasto = teclado.nextDouble();
+
+                System.out.println("Categorias: " );
+                System.out.println("1 - Alimentação " );
+                System.out.println("2 - Combustível " );
+                System.out.println("3 - Saúde " );
+                System.out.println("Digite a opção: " );
+                int opcao = teclado.nextInt();
+                switch(opcao){
+                    case 1:{
+                        totalAlimentacao += gasto;
+                        break;
+                    }
+                    case 2: {
+                        totalCombustivel += gasto;
+                        break;
+                    }
+                    case 3:{
+                        totalsaude += gasto;
+                        break;
+                    }
+                }
+
+                System.out.println("Deseja inserir mais um gasto: 1 - sim, 2 - não");
+                continuar = teclado.nextInt();
+
+            }while(continuar == 1);
+
+            double total = totalAlimentacao + totalCombustivel + totalsaude;
+            System.out.println("Total gasto no mês: " + total);
+
+            if(totalAlimentacao > totalCombustivel && totalAlimentacao > totalsaude){
+                System.out.println("Categoria com maior gasto foi alimentação.");
+                System.out.println("Gasto da categoria Alimentação: " + totalAlimentacao);
+            }
+            if(totalAlimentacao < totalCombustivel && totalCombustivel >totalsaude){
+                System.out.println("Categoria com maior gasto foi combustivel.");
+                System.out.println("Gasto da categoria combustivel: " + totalCombustivel);
+            }
+            if(totalsaude > totalCombustivel && totalAlimentacao < totalsaude){
+                System.out.println("Categoria com maior gasto foi saúde.");
+                System.out.println("Gasto da categoria saúde: " + totalsaude);
+            }
+            if(totalAlimentacao < totalCombustivel && totalAlimentacao < totalsaude){
+                System.out.println("Categoria com maior gasto foi alimentação.");
+                System.out.println("Gasto da categoria Alimentação: " + totalAlimentacao);
+            }
+            if(totalAlimentacao > totalCombustivel && totalCombustivel < totalsaude){
+                System.out.println("Categoria com maior gasto foi combustivel.");
+                System.out.println("Gasto da categoria combustivel: " + totalCombustivel);
+            }
+            if(totalsaude < totalCombustivel && totalAlimentacao < totalsaude){
+                System.out.println("Categoria com maior gasto foi saúde.");
+                System.out.println("Gasto da categoria saúde: " + totalsaude);
+            }
+            teclado.close();
+            }
+            
+        }
 }
