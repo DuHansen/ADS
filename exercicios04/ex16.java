0563ece5abdee7e5b24db5fd73660ae366a6da2b
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-05-08 20:52:15 -0300

    Update ex16.java

--- exercicios04/ex16.java
+++ exercicios04/ex16.java
@@ -7,4 +7,35 @@ ESSE MÊS".
  */
 public class ex16 {
     
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int continuar = 0, totalreceita = 0, totaldivida = 0;
+        
+        do{
+        System.out.println("Qual sua receita: ");
+        int receita = teclado.nextInt();
+        totalreceita += receita;
+
+        System.out.println("Qual sua despesa: ");
+        int divida = teclado.nextInt();
+            totaldivida += divida;
+
+        System.out.println("Deseja continuar 1 sim, 2 não : ");
+            continuar = teclado.nextInt();
+
+        }while(continuar == 1);
+
+        if(totaldivida > totalreceita){
+            System.out.println("Vocẽ está negativo!");
+        }
+        if(totaldivida < totalreceita){
+            System.out.println("Vocẽ está positivo!");
+        }
+        teclado.close();
+
+    }
+    
+}
+
+    
 }
