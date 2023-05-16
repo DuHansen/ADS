14dfb80fb7464e8759705bbef7e33f6ca6592dfe
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-05-15 21:17:36 -0300

    Create exercicios05.java

---
+++ exercicios05.java
@@ -0,0 +1,24 @@
+package array;
+import java.util.Scanner;
+
+
+public class ex01 {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        String[] vetor = new String[10];
+        
+        // entrada de dados
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println("Digite um número");
+            vetor[i] = teclado.nextLine();
+        }
+        
+        // saída de dados
+        for(int i = vetor.length-1; i >= 0; i--){
+            System.out.println(vetor[i]);
+
+        }
+        teclado.close();
+    
+    }}
