3a6df569e537e5e13693f93d35e2a76a224e7da3
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-05-17 19:54:44 -0300

    Update ex01.java

--- array/ex01.java
+++ array/ex01.java
@@ -1,9 +1,19 @@
 
 package array;
-import java.util.Scanner;
+import java.util.Scanner; 
+/*
+DESENVOLVA UM  PROGRAMA  QUE  LEIA E ARMAZENE 10 NOMES EM  UM ARRAY.
+APRESENTE  OS NOMES NA ORDEM DE LEITURA E DEPOIS NA ORDEM INVERSA DA LEITURA
+*/
+        //lenght = 4
+        // indice = 3
+        //OS ELEMENTOS DE UM ARRAY SÃO SEPARADOS POR VIRGULA
 
+        //System.out.println("Digite: " );
+        //double valor = teclado.nextDouble();
 
 public class ex01 {
+    
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
 
@@ -11,17 +21,28 @@ public class ex01 {
         
         // entrada de dados
         for(int i = 0; i < vetor.length; i++){
-            System.out.println("Digite um número");
+            System.out.println("Digite um nome:");
             vetor[i] = teclado.nextLine();
         }
-        
-        // saída de dados
-        for(int i = vetor.length-1; i >= 0; i--){
-            System.out.println(vetor[i]);
+
+        System.out.println();
+
+        // saída de dados em ordem de leitura
+        for(int i = 0; i < vetor.length; i++){
+            System.out.print(vetor[i]+ " ");
+
+        }
+
+        System.out.println();
+
+        // saída de dados em ordem decrescente de leitura
+        for(int i = vetor.length - 1; i >= 0; i--){
+            System.out.print(vetor[i] + " ");
 
         }
         teclado.close();
-    
-    }}
+    }
+}
+
 
     
