0baf31e05baf32f2c44d088b19afef5d22819020
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2023-05-17 19:55:21 -0300

    Update ex02.java

--- array/ex02.java
+++ array/ex02.java
@@ -1,26 +1,44 @@
 package array;
 import java.util.Scanner;
-/*
-ESCREVA  UM PROGRAMA  QUE LEIA10NÚMEROSE  ARMAZENE-OS EM  UM ARRAY.APÓS AS  LEITURAS  O PROGRAMA DEVE SOLICITAR AOUSUÁRIO UM NOVO NÚMERO.
-OPROGRAMA DEVEFAZER UMA BUSCA NO ARRAY E ESCREVER  A  MENSAGEM “ACHEI”,SE  O NÚMEROESTIVER  ENTRE  OS 10ARMAZENADOS,OU “NÃO ACHEI”CASO CONTRÁRIO.
+
+/* 
+ESCREVA  UM PROGRAMA  QUE LEIA 10 NÚMEROS E ARMAZENE-OS EM  
+UM ARRAY. APÓS AS  LEITURAS  O PROGRAMA DEVE SOLICITAR AO
+USUÁRIO UM NOVO NÚMERO. O PROGRAMA DEVE FAZER UMA BUSCA NO 
+ARRAY E ESCREVER  A  MENSAGEM “ACHEI”, SE  O NÚMERO ESTIVER 
+ENTRE  OS 10 ARMAZENADOS, OU “NÃO ACHEI” CASO CONTRÁRIO.
 */
+
 public class ex02 {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
-        
+
         int[] vetor = new int[10];
-
+        
+        // entrada de dados
         for(int i = 0; i < vetor.length; i++){
-
-            System.out.println("Digite: ");
+            System.out.println("Digite um número");
             vetor[i] = teclado.nextInt();
-
-            if(vetor[i] == vetor[i]){
-
-                System.out.println("Achou");
-
-            }
         }
 
-        teclado.close();
-}}
+
+        System.out.println("Informe o número da pesquisa: ");
+        int numero = teclado.nextInt();
+        boolean achei = false;
+
+
+        for(int i = 0; i < vetor.length; i++){
+            System.out.println(vetor[i]);
+            if(numero == vetor[i]){
+                achei = true;
+                System.out.println("Achei");
+                break;
+            }
+    }
+    if(!achei){
+        System.out.println("Achei");
+
+    }
+    teclado.close();
+}
+}
